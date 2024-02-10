ef6a9417117b5d593f6ba7cdcd6660e89a6b05b8
Jacob Bollmann <jacob.bollmann@yahoo.com>
2024-02-10 14:26:27 -0600

    added generic local state layout

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,11 +1,36 @@
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 function checkout() {
   const personInfo = useSelector((state) => state.customer);
   const itemInfo = useSelector((state) => state.cart);
+  const history = useHistory();
+
+  //for reference
+  //app.use('/api/order', orderRouter);
+  // const orderInsertResults = await client.query(`INSERT INTO "orders" ("customer_name", "street_address", "city", "zip", "type", "total")
+  // VALUES ($1, $2, $3, $4, $5, $6)
+
+  //Store info
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [zip, setZip] = useState('');
+  const [orderType, setOrderType] = useState('');
+  const [total, setTotal] = useState(0);
+
+  setName('');
+  setAddress('');
+  setCity('');
+  setZip('');
+  setOrderType('');
+  setTotal('');
 
   const checkoutSubmit = () => {
     //Do some stuff here then put on checkout button
+    history.push('/');
   };
 
   return (
