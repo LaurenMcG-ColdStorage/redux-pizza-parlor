ac163f7f8925e9308ef92fffd6b6041b6c668291
Jacob Bollmann <jacob.bollmann@yahoo.com>
2024-02-16 12:58:18 -0600

    Updated checkout component. Updated table to map through and show order items and added total state to track total

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,16 +1,17 @@
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 function checkout() {
   const history = useHistory();
 
-  const customerName = useSelector((state) => state.customer.name);
-  const customerAddress = useSelector((state) => state.customer.address);
-  const customerCity = useSelector((state) => state.customer.city);
-  const customerZip = useSelector((state) => state.customer.zip);
-  const itemName = useSelector((state) => state.cart.name);
-  const orderTotal = useSelector((state) => state.cart.cost);
+  const [total, setTotal] = useState();
+
+  const customerDetails = useSelector((state) => state.customer);
+  const orderDetails = useSelector((state) => state.cart);
+  //const itemName = useSelector((state) => state.cart.name);
+  //const orderTotal = useSelector((state) => state.cart.cost);
 
   //for reference
   //app.use('/api/order', orderRouter);
@@ -26,10 +27,10 @@ function checkout() {
     <div>
       <div>
         <p>
-          {customerName} <br />
-          {customerAddress} <br />
-          {customerCity} <br />
-          {customerZip}
+          {customerDetails.name} <br />
+          {customerDetails.address} <br />
+          {customerDetails.city} <br />
+          {customerDetails.zip}
         </p>
       </div>
       <div>
@@ -43,14 +44,19 @@ function checkout() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>{itemName}</td>
-            <td>{orderTotal}</td>
-          </tr>
+          {orderDetails.map((item, i) => {
+            <>
+              {setTotal(total + item.price)}
+              <tr key={i}>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+              </tr>
+            </>;
+          })}
         </tbody>
       </table>
       <div>
-        <p>Total: Numbers</p> <hr />
+        <p>Total: {total}</p> <hr />
         <button>CHECKOUT</button>
       </div>
     </div>
