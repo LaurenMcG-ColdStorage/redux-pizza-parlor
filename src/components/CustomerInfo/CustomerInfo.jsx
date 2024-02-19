7982a0aaa2aa9ac376676e5d695de190b809d249
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-02-18 21:00:24 -0500

    Fixed styling of customerInfo page

--- src/components/CustomerInfo/CustomerInfo.jsx
+++ src/components/CustomerInfo/CustomerInfo.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
+import './CustomerInfo.css';
+
 function CustomerInfo(){
 
     const [name, setName] = useState('');
@@ -31,19 +33,39 @@ function CustomerInfo(){
     return(
         <>
             <h2>Step Two: Customer Information</h2>
-            <label to='name'>Full Name</label>
-            <input name='name'  className='customerContent' type='text' placeholder="name" onChange={(event) => setName(event.target.value)} required></input><br />
-            <label to='address'>Street Address</label>
-            <input name='address' type='text' placeholder="street address" onChange={(event) => setAddress(event.target.value)} required></input><br />
-            <label to='city'>City</label>
-            <input name='city' type='text' placeholder="city" onChange={(event) => setCity(event.target.value)} required></input><br />
-            <label to='zip'>ZIP</label>
-            <input type='number' name='zip' placeholder="ZIP" onChange={(event) => setZip(event.target.value)} required></input><br />
-            <input name='servedType' type='radio' id='pickup' value='pickup' onChange={(event) => setType(event.target.value)}></input>
-            <label to='pickup' >Pick Up</label><br />
-            <input type='radio' name='servedType' value='delivery' onChange={(event) => setType(event.target.value)}></input>
-            <label to='delivery'>Delivery</label><br />
-            <button onClick={(event) => handleInfo(event)}>Next</button>
+            <table className='customerStyle'>
+                <tbody>
+                    <tr>
+                        <td><label to='name'>Full Name</label></td>
+                        <td><input name='name'  className='customerContent' type='text' placeholder="name" onChange={(event) => setName(event.target.value)} required></input></td>
+                    </tr>
+                    <tr>
+                        <td><label to='address'>Street Address</label></td>
+                        <td><input name='address' type='text' placeholder="street address" onChange={(event) => setAddress(event.target.value)} required></input></td>
+                    </tr>
+                    <tr>
+                        <td><label to='city'>City</label></td>
+                        <td><input name='city' type='text' placeholder="city" onChange={(event) => setCity(event.target.value)} required></input></td>
+                    </tr>
+                    <tr>
+                        <td><label to='zip'>ZIP</label></td>
+                        <td><input type='number' name='zip' placeholder="ZIP" onChange={(event) => setZip(event.target.value)} required></input></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <input name='servedType' type='radio' id='pickup' value='pickup' onChange={(event) => setType(event.target.value)}></input>
+                            <label to='pickup' > Pick Up </label>
+                            <input type='radio' name='servedType' value='delivery' onChange={(event) => setType(event.target.value)}></input>
+                            <label to='delivery'> Delivery</label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button onClick={(event) => handleInfo(event)}>Move to Checkout</button></td>
+                    </tr>
+                </tbody>
+            </table>
         </>
     )
 }
